eb2706d8a580298eef54ee72b66502c954332efa
Janne <janne@richen.fi>
2020-01-10 22:22:07 +0200

    Updates

---
+++ precache-manifest.01b0044a2646c5f378b2f404547768b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b150aba9b43c1f7653e0dd61d131f1",
+    "url": "/react-ui-stuff/index.html"
+  },
+  {
+    "revision": "c8053f59909d41b16f23",
+    "url": "/react-ui-stuff/static/css/main.f1731408.chunk.css"
+  },
+  {
+    "revision": "711d4352e2de30240a83",
+    "url": "/react-ui-stuff/static/js/2.dd643ac3.chunk.js"
+  },
+  {
+    "revision": "c8053f59909d41b16f23",
+    "url": "/react-ui-stuff/static/js/main.3cc0245d.chunk.js"
+  },
+  {
+    "revision": "b363890587d023596bff",
+    "url": "/react-ui-stuff/static/js/runtime~main.f2a34e63.js"
+  }
+]);
